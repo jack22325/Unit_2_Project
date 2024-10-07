91082e0f7fc0998057eefc71ff02186ce13ffb16
jack22325 <jacka49@nycstudents.net>
2024-10-07 09:53:22 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Enter the first coordinate point");
+        Scanner s = new Scanner(System.in);
+        String firstCoordinate = s.nextLine();
     }
 }
